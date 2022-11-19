111747c6e68c486d3238059299fc79a32df0e8d7
Bhargav644 <gautambhargav644@gmail.com>
2022-11-19 11:59:37 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Header from "./components/Header/Header";
 import { Fragment } from "react";
